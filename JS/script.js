d21ea79d8a5803af154e9acdcd78373fbd4d39bc
howarceus <howarceus@gmail.com>
2024-01-05 20:45:05 +0000

    Added event listener for the click, then stored it in local storage'

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 // Display the current day
 var currentDayEl = $("#currentDay");
 currentDayEl.text(dayjs().format('dddd, MMMM D, YYYY'));
+
+// Add event listener for save button
+$(".saveBtn").on("click", function () {
+    var hour = $(this).siblings("textarea").data("time");
+    var eventText = $(this).siblings("textarea").val();
+  
+    // Save to local storage
+    localStorage.setItem(hour, eventText);
+  });
